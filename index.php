b07f90917775a14bb3643f5bdbf0343aaad1bba3
holger1411 <office@holgerkoenemann.de>
2014-01-31 11:04:57 +0100

    Backlink URL´s changed

--- index.php
+++ index.php
@@ -499,13 +499,13 @@ if (document.documentElement.clientWidth >= 980) {
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 credits_left">
-					<p>Created and designed by <a href="http://www.pixelsparadise.com" target="_blank">Pixelsparadise.com</a><br />With help of <a href="http://twitter.github.com/bootstrap/">Bootstrap Framework</a> and Flexslider by <a href="http://flex.madebymufffin.com">Madebymufffin</a></p>
+					<p>Created and designed by <a href="http://www.joobstrap.com" target="_blank">joobstrap.com</a><br />With help of <a href="http://twitter.github.com/bootstrap/">Bootstrap Framework</a> and Flexslider by <a href="http://flex.madebymufffin.com">Madebymufffin</a></p>
 					<p class="copyright_by">&copy;  <?php echo date('Y');?> by <a href="<?php echo $this->baseurl;?>"><?php echo $app->getCfg('sitename'); ?></a></p>
 				</div>
 			
 				<div class="col-md-6 credits_right">
 				<p class="text-align-right"><a href="http://www.joomla.org" title="Click here to visit Joomla!" target="_blank">Joomla! ®</a> name is used under a limited license from <a href="http://opensourcematters.org" title="Click here to visit Open Source Matters" target="_blank">Open Source Matters</a><br>
-				<a title="pixelsparadise.com" href="http://www.pixelsparadise.com">Pixelsparadise.com</a> is not affiliated with the <a title="Joomla! Home" href="http://www.joomla.org">Joomla!</a> Project.</p>
+				<a title="pixelsparadise.com" href="http://www.joobstrap.com">Joobstrap.com</a> is not affiliated with the <a title="Joomla! Home" href="http://www.joomla.org">Joomla!</a> Project.</p>
 				<p class="back_to_the_top text-align-right"><a href="#to_the_top" id="back-top"><i class="icon-circle-arrow-up icon-white"></i> Back to top</a></p>
 				</div>
 			</div>
